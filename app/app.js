1598b45075456ac2b32461b4a373e3c43751412b
vladgaranich <vladislav.garanich@gmail.com>
2019-01-05 16:53:35 +0200

    first commit

---
+++ app/app.js
@@ -0,0 +1,5 @@
+
+document.querySelector(".hamburguer").addEventListener("click", function () {
+    document.querySelector(".full-menu").classList.toggle("active");
+    document.querySelector(".hamburguer").classList.toggle("close-hamburguer");
+});
---
+++ app/script.js
@@ -0,0 +1,7 @@
+
+document.querySelector("#menu-button").addEventListener("click", function () {
+    document.querySelector(".header-menu-mobile").classList.toggle("active");
+});
+document.querySelector("#js-close-header-menu").addEventListener("click", function () {
+    document.querySelector(".header-menu-mobile").classList.toggle("active");
+});
